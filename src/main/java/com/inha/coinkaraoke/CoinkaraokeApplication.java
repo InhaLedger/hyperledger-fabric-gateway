10b840ae7080f30b4839de2d4b86cce180a75bc3
Dong-Hyeon-Yu <ydh9516@gmail.com>
2022-10-07 15:43:14 +0900

    init

---
+++ src/main/java/com/inha/coinkaraoke/CoinkaraokeApplication.java
@@ -0,0 +1,13 @@
+package com.inha.coinkaraoke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinkaraokeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinkaraokeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inha/coinkaraoke/CoinkaraokeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inha.coinkaraoke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinkaraokeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
